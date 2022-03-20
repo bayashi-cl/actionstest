cf337b7eaf07538bbebcd6f304b43657a8d9e825
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-03-21 04:59:08 +0900

    verify normal

--- acl.test.cpp
+++ acl.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
 #include <bits/stdc++.h>
 
-#include <atcoder/modint>
+// #include <atcoder/modint>
 using namespace std;
 int main() { cout << "Hello World" << endl; }
