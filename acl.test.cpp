bf2d9d7f25ad26061f956d132966a62161b16963
Masaki Kobayashi <bayashi.cl@gmail.com>
2024-03-02 07:56:28 +0000

    del old

--- acl.test.cpp
+++
@@ -1,6 +0,0 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
-#include <bits/stdc++.h>
-
-#include <atcoder/modint>
-using namespace std;
-int main() { cout << "Hello World" << endl; }
